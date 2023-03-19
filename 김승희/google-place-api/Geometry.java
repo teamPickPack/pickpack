c0071d45dec18d6c4e4a388eac6c4472b73aeba7
김승희 <kanada11416@gmail.com>
2023-03-19 23:22:58 +0900

    add : google place api code

---
+++ 김승희/google-place-api/Geometry.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Geometry {
+    Location location;
+    Object viewport;
+
+    public Geometry(Location location, Object viewport) {
+        this.location = location;
+        this.viewport = viewport;
+    }
+}
---
+++ 김승희/google-place-api/Location.java
@@ -0,0 +1,9 @@
+public class Location {
+    String lat;
+    String lng;
+
+    public Location(String lat, String lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
