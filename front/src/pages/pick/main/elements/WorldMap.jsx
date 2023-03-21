dc7f1f5b919963bdd97d01105cd6f38b44c7819e
신현광 <rhkd0363@gmail.com>
2023-03-21 21:12:10 +0900

    feat: 메인화면 지도 추가

--- front/src/pages/pick/main/elements/WorldMap.jsx
+++ front/src/pages/pick/main/elements/WorldMap.jsx
@@ -1,5 +1,19 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 const WorldMap = () => {
-  return <div></div>;
+  const position = [51.505, -0.09];
+  return (
+    <>
+      <MapContainer center={position} zoom={2} style={{ height: "400px" }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={position}>
+          <Popup>팝업</Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 };
 
 export default WorldMap;
