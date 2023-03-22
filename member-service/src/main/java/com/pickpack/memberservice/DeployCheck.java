b56c7592470473f7522f9445813c047364e87d65
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-22 14:24:15 +0900

    feat: member-service deploy check

---
+++ member-service/src/main/java/com/pickpack/memberservice/DeployCheck.java
@@ -0,0 +1,16 @@
+package com.pickpack.memberservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member/")
+public class DeployCheck {
+
+    @GetMapping("/check")
+    public String welcomeCheck(){
+        return "welcome to member-service";
+    }
+
+}
