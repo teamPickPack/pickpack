3fa9cc4843d68bcb7215d3bd5399de50128dc1b7
이상민 <lsms3723@daum.net>
2023-03-24 17:58:59 +0900

    feat:file upload handler

--- chat-service/src/main/java/com/pickpack/chatservice/config/WebSockConfig.java
+++ chat-service/src/main/java/com/pickpack/chatservice/config/WebSockConfig.java
@@ -1,5 +1,6 @@
 package com.pickpack.chatservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private AgentWebSocketHandlerDecoratorFactory agentWebSocketHandlerDecoratorFactory;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -22,8 +25,11 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*");
 //                .withSockJS();
     }
+
+    //TODO 안 먹음
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setMessageSizeLimit(50 * 1024 * 1024);
+        registration.setDecoratorFactories(agentWebSocketHandlerDecoratorFactory);
+        registration.setSendBufferSizeLimit(512*1024*1024);
     }
 }
