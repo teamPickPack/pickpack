41c11d0a6e68486b147e6ec69373e1bd9e62a603
김아린 <mwtgal20@gmail.com>
2023-03-25 23:41:41 +0900

    feat : 항공권 편도 검색 구현

--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepository.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepository.java
@@ -3,5 +3,35 @@ package com.pickpack.flightservice.repository.ticket;
 import com.pickpack.flightservice.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    //priceRow & 직항
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIs0AndPriceBetweenOOrderByPriceAsc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //TODO : gapHigh & 직항
+
+    //depTimeEarly & 직항
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIs0AndPriceBetweenOOrderByDepTimeAsc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //depTimeLate & 직항
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIs0AndPriceBetweenOOrderByDepTimeDesc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //flightTimeRow & 직항
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIs0AndPriceBetweenOOrderByTotalTimeNumAsc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //priceRow & 경유
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIsNot0AndPriceBetweenOOrderByPriceAsc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //TODO : gapHigh & 경유
+
+    //depTimeEarly & 경유
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIsNot0AndPriceBetweenOOrderByDepTimeAsc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //depTimeLate & 경유
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIsNot0AndPriceBetweenOOrderByDepTimeDesc(String departure, String destination, String date, int minPrice, int maxPrice);
+
+    //flightTimeRow & 경유
+    List<Ticket> findByDepNameAndArrNameAndDepDateAndWaypointNumIsNot0AndPriceBetweenOOrderByTotalTimeNumAsc(String departure, String destination, String date, int minPrice, int maxPrice);
+
 }
--- flight-service/src/main/java/com/pickpack/flightservice/service/ticket/TicketService.java
+++ flight-service/src/main/java/com/pickpack/flightservice/service/ticket/TicketService.java
@@ -1,4 +1,11 @@
 package com.pickpack.flightservice.service.ticket;
 
+import com.pickpack.flightservice.api.request.TicketReq;
+import com.pickpack.flightservice.api.response.TicketRes;
+import com.pickpack.flightservice.entity.Ticket;
+
+import java.util.List;
+
 public interface TicketService {
+    List<TicketRes> getTicketList(TicketReq ticketReq);
 }
