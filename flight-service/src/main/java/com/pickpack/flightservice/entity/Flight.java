10ffde8da47c034beb1136d2ebc2acae1884d8c6
김아린 <mwtgal20@gmail.com>
2023-03-25 18:07:34 +0900

    chore : Flight 누락 필드 추가 및 lombok 추가

--- flight-service/src/main/java/com/pickpack/flightservice/entity/Flight.java
+++ flight-service/src/main/java/com/pickpack/flightservice/entity/Flight.java
@@ -1,8 +1,16 @@
 package com.pickpack.flightservice.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Flight {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +30,7 @@ public class Flight {
     private String flightTime;
     private String code;
     private String waypointName;
+    private int plusDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ticket_id")
