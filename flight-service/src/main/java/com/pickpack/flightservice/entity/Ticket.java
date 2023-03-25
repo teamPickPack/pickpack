edd7ad7dc9b274b3526e9b032973f59e3f5a1f75
김아린 <mwtgal20@gmail.com>
2023-03-25 18:08:09 +0900

    chore : Ticket Entity lombok 추가

--- flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
+++ flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
@@ -1,9 +1,17 @@
 package com.pickpack.flightservice.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
