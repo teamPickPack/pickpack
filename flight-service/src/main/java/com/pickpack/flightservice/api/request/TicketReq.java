5eabcb704c1a3baa6d6ef80a8854ebb036d8f941
김아린 <mwtgal20@gmail.com>
2023-03-26 23:31:00 +0900

    chore : Dto Lombok annotation 추가

--- flight-service/src/main/java/com/pickpack/flightservice/api/request/TicketReq.java
+++ flight-service/src/main/java/com/pickpack/flightservice/api/request/TicketReq.java
@@ -2,13 +2,11 @@ package com.pickpack.flightservice.api.request;
 
 import com.pickpack.flightservice.dto.flight.FilterDto;
 import com.pickpack.flightservice.dto.flight.OneWayInfoDto;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class TicketReq {
     private long memberId;
     private OneWayInfoDto info;
