7d9743d158197f28f252775c18ad4d10f8f1b973
최희수 <93choiheesoo@naver.com>
2023-03-27 19:30:53 +0900

    Revert "Merge branch 'feature/MS-item_service' into 'master'"
    This reverts merge request !21

--- member-service/src/main/java/com/pickpack/memberservice/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pickpack.memberservice.repository;
-
-import com.pickpack.memberservice.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
-}
