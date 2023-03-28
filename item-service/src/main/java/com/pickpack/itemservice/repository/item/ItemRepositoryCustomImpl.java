d8f2315318eef7b7e2ef7e4c0c97530729a9b0cc
seunghee <kanada11416@gmail.com>
2023-03-29 02:16:06 +0900

    feat : category 별 item list GET

---
+++ item-service/src/main/java/com/pickpack/itemservice/repository/item/ItemRepositoryCustomImpl.java
@@ -0,0 +1,41 @@
+package com.pickpack.itemservice.repository.item;
+
+import com.pickpack.itemservice.dto.item.ItemListDto;
+import com.pickpack.itemservice.entity.Category;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.pickpack.itemservice.entity.QCity.city;
+import static com.pickpack.itemservice.entity.QItem.item;
+import static com.pickpack.itemservice.entity.QMember.member;
+
+public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
+    private final JPAQueryFactory queryFactory;
+
+    public ItemRepositoryCustomImpl(EntityManager em){
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+    public List<ItemListDto> getItemsWithCategory(String categoryStr) {
+        List<ItemListDto> itemsWithCategory =
+                queryFactory.select(Projections.fields(ItemListDto.class,
+                        item.id.as("itemId"),
+                        member.id.as("memberId"),
+                        item.title,
+                        item.category,
+                        item.price,
+                        item.itemName,
+                        item.imgUrl,
+                        item.registDate,
+                        city.id.as("cityId"),
+                        city.cityName)).from(item)
+                        .join(item.member, member)
+                        .join(item.city, city)
+                        .where(item.category.eq(Category.valueOf(categoryStr)))
+                        .where(item.isComplete.eq(Boolean.FALSE))
+                        .where(item.isDelete.eq(Boolean.FALSE)).fetch();
+        return itemsWithCategory;
+    }
+}
