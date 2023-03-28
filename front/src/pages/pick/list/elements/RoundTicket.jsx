b31c375815c1248e37765403561746ce97b267c1
허윤회 <dbsghl123@naver.com>
2023-03-28 13:14:44 +0900

    feat : 비교하기 사이드바 완료

--- front/src/pages/pick/list/elements/RoundTicket.jsx
+++ front/src/pages/pick/list/elements/RoundTicket.jsx
@@ -1,11 +1,21 @@
 import {useState, useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { compareAction } from '../../../../store/compareSlice';
 import styled from 'styled-components';
 import OneWayTicket from "./OneWayTicket"
 
-export default function RoundTicket({isCheck, isLike, goWay, returnWay, totalPrice}){
-
+export default function RoundTicket({
+    fromCompare,
+    handleLikeData,
+    isCheck,
+    isLike,
+    goWay,
+    returnWay,
+    totalPrice
+}){
     useEffect(() => {
-        const list = document.querySelectorAll('.tickets > div')
+        const list = document.querySelectorAll('.tickets > .ticket')
+        // console.log(list);
         list.forEach((el, index) => {
             el.style.zIndex = index  
             el.addEventListener('click', function(){
@@ -21,33 +31,122 @@ export default function RoundTicket({isCheck, isLike, goWay, returnWay, totalPri
                 array.forEach((el, index) => {
                     el.style.zIndex = zIndexArray[index]
                 })
+                for(let i = 0; i < zIndexArray.length; i+=2){
+                    console.log(zIndexArray[i] < zIndexArray[i+1]);
+                    if(zIndexArray[i] < zIndexArray[i+1]){
+                        list[i].style.opacity = '.7';
+                        list[i+1].style.opacity = '1';
+                    } else{
+                        list[i].style.opacity = '1';
+                        list[i+1].style.opacity = '.7';
+                    }
+                }
             })
         })
         let array = [...list]
         let zIndexArray = array.map(arr => Number(arr.style.zIndex))
+        for(let i = 0; i < zIndexArray.length; i+=2){
+            if(zIndexArray[i] < zIndexArray[i+1]){
+                list[i].style.opacity = '.5';
+                list[i+1].style.opacity = '1';
+            } else{
+                list[i].style.opacity = '1';
+                list[i+1].style.opacity = '.5';
+            }
+        }
     }, []);
 
-    const [commonAlarmChecked, setCommonAlarmChecked] = useState(isLike);
-    const [commonCompareChecked, setCommonCompareChecked] = useState(isCheck);
-    const unifyChecked = (type) => {
-        if(type === 'compare'){
-            console.log('compareChecked 동기화하기')
-            // setUnifyCompareChecked((unifyCompareChecked) => !unifyCompareChecked);
+    const [commonLike, setCommonLike] = useState(isLike);
+    const [commonCheck, setCommonCheck] = useState(isCheck);
+
+    const compareList = useSelector((state)=>{
+        return state.compare.compareList;
+    });
+    const compareMode = useSelector((state) => {
+        return state.compare.compareMode;
+    });
+
+    useEffect(() => {
+        setCommonCheck(isCheck);
+    }, [isCheck])
+    useEffect(() => {
+        setCommonLike(isLike);
+    }, [isLike])
+    const dispatch = useDispatch();
+    
+    const unifyCommon = (type) => {
+        if(type === 'check'){
+            //여기서도 세션 스토리지에 접근해서 데이터 넣어주고 빼줘야 함
+            const payload = {
+                mode: 'round',
+                isLike,
+                flightId: `${goWay.ticket.ticketId}-${returnWay.ticket.ticketId}`,
+                flightData: [
+                    {
+                        ticket: goWay.ticket,
+                        flightList: goWay.flightList,
+                    },
+                    {
+                        ticket: returnWay.ticket,
+                        flightList: returnWay.flightList,
+                    },
+                ]
+            }
+            if(!commonCheck){
+                if(compareList.length !== 0 && compareMode !== 'round'){
+                    alert('편도와 왕복을 동시에 담을 순 없어요,,,,');
+                    return;
+                }
+                else if(compareList.length >= 5){
+                    alert('다섯개 꽉 찼으요...');
+                    return;
+                }
+                setCommonCheck(true);
+                dispatch(compareAction.addCompareItem(payload))
+            }
+            else{
+                setCommonCheck(false);
+                dispatch(compareAction.deleteCompareItem(payload))
+            }
         } else{
-            console.log('alarmChecked 동기화하기');
-            if(commonAlarmChecked) alert('알림이 해제되었습니다.');
-            else alert('알림이 등록되었습니다.');
-            setCommonAlarmChecked((commonAlarmChecked) => !commonAlarmChecked);
+            const payload = {
+                mode: 'round',
+                isLike: !commonLike,
+                flightId: `${goWay.ticket.ticketId}-${returnWay.ticket.ticketId}`,
+                flightData: [
+                    {
+                        ticket: goWay.ticket,
+                        flightList: goWay.flightList,
+                    },
+                    {
+                        ticket: returnWay.ticket,
+                        flightList: returnWay.flightList,
+                    },
+                ]
+            }
+            if(commonLike) {
+                alert('알림이 해제되었습니다.');
+            }
+            else {
+                alert('알림이 등록되었습니다.');
+            }
+            if(commonCheck){
+                handleLikeData(payload.flightId, !commonLike);
+                dispatch(compareAction.updateCompareItem(payload));
+            }
+            else{
+                setCommonLike((commonLike) => !commonLike);
+            }
         }
     }
     return(
         <>
         <Tickets className="tickets">
-            <ReturnTicket>
-                <OneWayTicket unifyChecked={unifyChecked} twoWay={true} isLike={commonAlarmChecked} ticket={returnWay.ticket} flightList={returnWay.flightList}/>
+            <ReturnTicket className="ticket">
+                <OneWayTicket fromCompare={fromCompare} isRound={true} unifyCommon={unifyCommon} handleLikeData={handleLikeData} isCheck={commonCheck} isLike={commonLike} ticket={returnWay.ticket} flightList={returnWay.flightList}/>
             </ReturnTicket>
-            <GoTicket>
-                <OneWayTicket unifyChecked={unifyChecked} twoWay={true} isLike={commonAlarmChecked} ticket={goWay.ticket} flightList={goWay.flightList}/>
+            <GoTicket className="ticket">
+                <OneWayTicket fromCompare={fromCompare} isRound={true} unifyCommon={unifyCommon} handleLikeData={handleLikeData} isCheck={commonCheck} isLike={commonLike} ticket={goWay.ticket} flightList={goWay.flightList}/>
             </GoTicket>
             <TotalPrice>총 2,640,000원</TotalPrice>
         </Tickets>
