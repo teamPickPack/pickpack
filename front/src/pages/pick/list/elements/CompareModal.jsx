2654729d2c6c0ade8f81c2b7032730c668d45a16
허윤회 <dbsghl123@naver.com>
2023-03-29 17:56:28 +0900

    feat: 항공권 비교 테이블 추가

--- front/src/pages/pick/list/elements/CompareModal.jsx
+++ front/src/pages/pick/list/elements/CompareModal.jsx
@@ -1,61 +1,28 @@
-import { useSelector, useDispatch } from 'react-redux';
-import FlightList from './FlightList';
+import { useSelector } from 'react-redux';
+import CompareModalItem from './CompareModalItem';
 export default function CompareModal(){
+    const color = ['#FF0013', 'orange', '#1CC500', '#72A2FF', 'purple'];
     const compareList = useSelector((state)=>{
         return state.compare.compareList;
     });
-    const compareMode = useSelector((state) => {
-        return state.compare.compareMode;
-    });
     return(
-        <div style={{border: '3px solid red'}}>
-            {compareList.map((compareItem) => (
-                <li key={compareItem.flightId}>{compareItem.flightId}</li>
-            ))}
+        <div style={{border: '3px solid red', textAlign: 'center', display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
+            <h2>항공권 비교 테이블</h2>
             <table style={{borderCollapse: 'collapse'}}>
                 <thead>
                     <tr>
-                        <th  style={{border: '1px solid black'}}>출발정보</th>
-                        <th  style={{border: '1px solid black'}}>도착정보</th>
-                        <th  style={{border: '1px solid black'}}>경유정보</th>
-                        <th  style={{border: '1px solid black'}}>소요시간</th>
-                        <th  style={{border: '1px solid black'}}>항공사</th>
-                        <th  style={{border: '1px solid black'}}>가격</th>
-                        <th  style={{border: '1px solid black'}}>상세정보</th>
+                        <th  style={{border: '1px solid black', width: '200px'}}>출발정보</th>
+                        <th  style={{border: '1px solid black', width: '200px'}}>도착정보</th>
+                        <th  style={{border: '1px solid black', width: '200px'}}>경유정보</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>소요시간</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>항공사</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>가격</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>상세정보</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {compareList.map((compareItem) => (
-                        <>
-                            <tr>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>인천(ICN)</div>
-                                    <div>2023.03.23 12:40</div>
-                                </td>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>인천(ICN)</div>
-                                    <div>2023.03.23 12:40</div>
-                                </td>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>경유 2회</div>
-                                    <div>프랑크푸르트(FRA)</div>
-                                    <div>이스탄불(IST)</div>
-                                </td>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>33시간 50분</div>
-                                </td>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>대한항공</div>
-                                </td>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>823,000원</div>
-                                </td>
-                                <td style={{border: '1px solid gray', height: '160px'}}>
-                                    <div>펼치기</div>
-                                </td>
-                            </tr>
-                            <FlightList/>
-                        </>
+                    {compareList.map((compareItem, index) => (
+                       <CompareModalItem key={compareItem.flightId} item={compareItem} color={color[index]}/>
                     ))}
                 </tbody>
             </table>
