502c6c6b4369020c1ae1d2ba5d584e2187b57eac
seunghee <kanada11416@gmail.com>
2023-03-29 23:10:21 +0900

    chore : item 상세 정보와 작성자의 다른 글 GET

--- item-service/src/main/java/com/pickpack/itemservice/repository/item/ItemRepositoryCustom.java
+++ item-service/src/main/java/com/pickpack/itemservice/repository/item/ItemRepositoryCustom.java
@@ -1,12 +1,18 @@
 package com.pickpack.itemservice.repository.item;
 
+import com.pickpack.itemservice.dto.item.ItemDetailDto;
 import com.pickpack.itemservice.dto.item.ItemListDto;
 import com.pickpack.itemservice.entity.Category;
+import com.pickpack.itemservice.entity.Item;
+import com.pickpack.itemservice.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepositoryCustom {
     List<ItemListDto> getItemsWithCategory(String categoryStr);
     List<ItemListDto> getItemsSearchOnTitle(String categoryStr, String search);
     List<ItemListDto> getItemsSearchOnCity(String categoryStr, Long cityId);
+    List<ItemListDto> getItemsByMember(Long itemId, Long memberId);
+    ItemDetailDto getItemById(Long itemId);
 }
