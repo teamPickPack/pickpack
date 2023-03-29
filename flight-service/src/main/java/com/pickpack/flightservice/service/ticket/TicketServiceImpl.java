5e8ae11f2a302a1453d69f77bd9d25d3d9491b7b
김아린 <mwtgal20@gmail.com>
2023-03-29 10:00:07 +0900

    feat : 항공권 검색 왕복

--- flight-service/src/main/java/com/pickpack/flightservice/service/ticket/TicketServiceImpl.java
+++ flight-service/src/main/java/com/pickpack/flightservice/service/ticket/TicketServiceImpl.java
@@ -38,22 +38,19 @@ public class TicketServiceImpl implements TicketService {
 
         PageRequest pageRequest = PageRequest.of(page, 10, Sort.Direction.fromString(orderBy), sortType);
 
+        //검색
         List<Ticket> ticketList = new ArrayList<>();
 
-        //검색
         if(direct[0]) { //경유 필터 : 전체
-            System.out.println("전체 검색");
             ticketList = ticketRepository.findAllTickets(pageRequest, departure, destination, date, minPrice, maxPrice);
         }else { //경유 필터 : 직항, 경유 1회, 경유 2회 이상
             for(int i = 1;  i < direct.length; i++) {
                 List<Ticket> tmp = new ArrayList<>();
 
                 if(direct[i] && (i == 1 || i == 2)) { //직항, 경유 1회
-                    System.out.println("직항, 경유 1회");
                     tmp = ticketRepository.findWaypoint0or1Tickets(pageRequest, departure, destination, date, minPrice, maxPrice, i - 1);
                 } else if(direct[i] && (i == 3)) { //경유 2회 이상
-                    System.out.println("경유 2회 이상");
-                    tmp = ticketRepository.findWaypointIsGraterThan1Tickets(pageRequest, departure, destination, date, minPrice, maxPrice);
+                    tmp = ticketRepository.findWaypointIsGraterThanTickets(pageRequest, departure, destination, date, minPrice, maxPrice, 1);
                 }
 
                 if(tmp != null) ticketList.addAll(tmp);
@@ -89,14 +86,102 @@ public class TicketServiceImpl implements TicketService {
         boolean direct[] = ticketReq.getFilter().getDirect();
         int minPrice = ticketReq.getFilter().getMinPrice();
         int maxPrice = ticketReq.getFilter().getMaxPrice();
-        String sortType = ticketReq.getSortType();
+        //패이징 정보
+        String sortType = ticketReq.getPageable().getSortType();
+        String orderBy = ticketReq.getPageable().getOrderBy();
+        int page = ticketReq.getPageable().getPage();
+
+        PageRequest pageRequest = PageRequest.of(page, 10, Sort.Direction.fromString(orderBy), sortType);
 
         //검색
+        List<RoundTicketRes> result = new ArrayList<>();
 
+        List<Ticket> goWayTicketList = new ArrayList<>();
+        List<Ticket> returnWayTicketList = new ArrayList<>();
+
+        if(direct[0]) { //경유 필터 : 전체
+            goWayTicketList = ticketRepository.findAllTickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice);
+            returnWayTicketList = ticketRepository.findAllTickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice);
+        }else { //경유 필터 : 직항, 경유 1회, 경유 2회 이상
+            for(int i = 1;  i < direct.length; i++) {
+//                List<Ticket> tmpGoWay = new ArrayList<>();
+//                List<Ticket> tmpReturnWay = new ArrayList<>();
+
+                List<RoundTicketRes> tmp = new ArrayList<>();
+
+                //TODO : 조합의 수마다 또 항공권 조합해야함
+                if(direct[i] && (i == 1)) { //직항
+                    goWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice, 0);
+                    returnWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice, 0);
+                
+                    //조합하기
+                    tmp = combine(goWayTicketList, returnWayTicketList);
+                    result.addAll(tmp);
+                } else if(direct[i] && (i == 2)) { //경유 1회
+                    //출국편 경유 0회, 귀국편 경유 1회
+                    goWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice, 0);
+                    returnWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice, 1);
+
+                    tmp = combine(goWayTicketList, returnWayTicketList);
+                    result.addAll(tmp);
+
+                    //출국편 경유 1회, 귀국편 경유 0회
+                    goWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice, 1);
+                    returnWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice, 0);
+
+                    tmp = combine(goWayTicketList, returnWayTicketList);
+                    result.addAll(tmp);
+                }
+                else if(direct[i] && (i == 3)) { //경유 2회 이상
+                    //출국편 경유 0회, 귀국편 경유 2회 이상
+                    goWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice, 0);
+                    returnWayTicketList = ticketRepository.findWaypointIsGraterThanTickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice, 1);
+
+                    tmp = combine(goWayTicketList, returnWayTicketList);
+                    result.addAll(tmp);
+
+                    //출국편 경유 2회 이상, 귀국편 경유 0회
+                    goWayTicketList = ticketRepository.findWaypointIsGraterThanTickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice, 1);
+                    returnWayTicketList = ticketRepository.findWaypoint0or1Tickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice, 0);
+
+                    tmp = combine(goWayTicketList, returnWayTicketList);
+                    result.addAll(tmp);
+
+                    //출국편 경유 1회 이상, 귀국편 경유 1회
+                    goWayTicketList = ticketRepository.findWaypointIsGraterThanTickets(pageRequest, "ICN", destination, depDate, minPrice, maxPrice, 0);
+                    returnWayTicketList = ticketRepository.findWaypointIsGraterThanTickets(pageRequest, destination, "ICN", arrDate, minPrice, maxPrice, 0);
+
+                    tmp = combine(goWayTicketList, returnWayTicketList);
+                    result.addAll(tmp);
+                }
+            }
+        }
 
         //결과값 반환
+        return result;
+    }
 
-        return null;
+    private List<RoundTicketRes> combine(List<Ticket> goWayTicketList, List<Ticket> returnWayTicketList) {
+        List<RoundTicketRes> result = new ArrayList<>();
+
+       for(Ticket goWayTicket : goWayTicketList) {
+           goWayTicket.setWaypoints(wayPointToString(goWayTicket.getFlightList()));
+
+           for(Ticket returnWayTicket : returnWayTicketList) {
+               returnWayTicket.setWaypoints(wayPointToString(returnWayTicket.getFlightList()));
+
+               RoundTicketRes ticketRes = RoundTicketRes.builder().
+                       isLike(false) //TODO : isLike 일단 임시로 false
+                       .goWay(goWayTicket)
+                       .returnWay(returnWayTicket)
+                       .totalPrice(goWayTicket.getPrice() + returnWayTicket.getPrice())
+                       .build();
+
+               result.add(ticketRes);
+           }
+       }
+
+        return result;
     }
 
     private String wayPointToString(List<Flight> flightList) { //경유지 정해진 형식으로 변환
