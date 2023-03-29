24d958d57c1e8b8092dffdb28a978421880a9204
이상민 <lsms3723@daum.net>
2023-03-29 19:30:17 +0900

    feat: delete trash file after merge

--- chat-service/src/main/java/com/pickpack/chatservice/entity/ChatRoom.java
+++ chat-service/src/main/java/com/pickpack/chatservice/entity/ChatRoom.java
@@ -1,21 +1,39 @@
 package com.pickpack.chatservice.entity;
 
+import lombok.Getter;
+
 import javax.persistence.*;
-import java.util.List;
+import java.io.Serializable;
+import java.sql.Timestamp;
 
+// TODO chatroom에 대한 id 수정(room_id+아이디)<-왜 그랬더라?
 @Entity
-public class Chatroom {
+@Getter
+@Table(name="chat_room")
+public class ChatRoom implements Serializable {
+//    private static final long serialVersionUID = -3377559815188666211L;
 
+    @Column(name = "room_id")
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "chatroom_id")
-    private Long id;
+    private String roomId;
 
-    @OneToMany(mappedBy = "chatroom")
-    private List<MemberChatroom> memberChatroomList;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
+    @ManyToOne
+    @JoinColumn(name="item_id")
     private Item item;
-}
 
+    //TODO Member에 serializable 걸건지 연관관계를 끊을건지 결정
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "seller_name",referencedColumnName ="nickname")
+    private String seller;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "buyer_name",referencedColumnName ="nickname")
+    private String buyer;
+//    @Column
+//    private String itemName;
+//    private String imgUrl;
+    private int messageSize;
+    private String lastMessage;
+    private boolean isNew;
+    @Column(name = "recent_time")
+    private Timestamp lastMessageTime;
+}
