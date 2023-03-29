dff60064833565a3910b55c69fa17ca19ad7f75e
이상민 <lsms3723@daum.net>
2023-03-29 16:43:06 +0900

    feat: modify controller

--- chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatMessageService.java
+++ chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatMessageService.java
@@ -2,10 +2,13 @@ package com.pickpack.chatservice.service.chat;
 
 import com.pickpack.chatservice.entity.redis.RedisChatMessage;
 
+import java.util.List;
+
 public interface ChatMessageService {
     void createMessage(RedisChatMessage message);
 
     void redisChatMessageWarming();
 
     void sendMessageToDB();
+    List<RedisChatMessage> getMessages(String roomId);
 }
