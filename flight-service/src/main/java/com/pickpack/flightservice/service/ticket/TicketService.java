bca3d71050c2c1c2ea54141abad42897ff57a68f
김아린 <mwtgal20@gmail.com>
2023-03-31 22:45:12 +0900

    fix : build error 해결

--- flight-service/src/main/java/com/pickpack/flightservice/service/ticket/TicketService.java
+++ flight-service/src/main/java/com/pickpack/flightservice/service/ticket/TicketService.java
@@ -1,6 +1,6 @@
 package com.pickpack.flightservice.service.ticket;
 
-import com.pickpack.flightservice.api.request.OneWayTicketReq;
+import com.pickpack.flightservice.api.request.OnewayTicketReq;
 import com.pickpack.flightservice.api.request.RoundTicketReq;
 import com.pickpack.flightservice.api.response.OneWayTicketRes;
 import com.pickpack.flightservice.api.response.RoundTicketRes;
@@ -8,6 +8,6 @@ import com.pickpack.flightservice.api.response.RoundTicketRes;
 import java.util.List;
 
 public interface TicketService {
-    List<OneWayTicketRes> getOneWayTicketList(OneWayTicketReq ticketReq);
+    List<OneWayTicketRes> getOneWayTicketList(OnewayTicketReq ticketReq);
     List<RoundTicketRes> getRoundTicketList(RoundTicketReq ticketReq);
 }
