9abf3c232829039d8f5973cb588d0e56737cb3df
이상민 <lsms3723@daum.net>
2023-04-02 16:32:28 +0900

    fix : set endpoint to connect nginx3

--- chat-service/src/main/java/com/pickpack/chatservice/config/WebSockConfig.java
+++ chat-service/src/main/java/com/pickpack/chatservice/config/WebSockConfig.java
@@ -27,7 +27,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws-stomp").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*");
 //                .withSockJS();
     }
 
