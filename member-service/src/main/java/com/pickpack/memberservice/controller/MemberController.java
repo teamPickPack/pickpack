4005dca3a586bc79d4eb761da076d684ec851d98
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-03 16:12:03 +0900

    feat: logback config

--- member-service/src/main/java/com/pickpack/memberservice/controller/MemberController.java
+++ member-service/src/main/java/com/pickpack/memberservice/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.pickpack.memberservice.exception.custom.NotExistMIdException;
 import com.pickpack.memberservice.exception.custom.NotExistNicknameException;
 import com.pickpack.memberservice.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -31,8 +33,9 @@ public class MemberController {
 
         if(joinReqDto.getNickname().length() == 0) throw new NotExistNicknameException("닉네임를 입력하지 않음");
         if(joinReqDto.getMid().length() == 0) throw new NotExistMIdException("아이디을 입력하지 않음");
-
         JoinRespDto joinRespDto = memberService.join(joinReqDto);
+        log.info("회원가입 완료, 회원 - {}", joinReqDto.getNickname());
+
         return new ResponseEntity<>(joinRespDto, HttpStatus.CREATED);
     }
 
