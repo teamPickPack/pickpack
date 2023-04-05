94024b8f596379278a1b61b713681eaac9629277
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-05 16:47:15 +0900

    chore: add log for tourist

--- flight-service/src/main/java/com/pickpack/flightservice/controller/tourist/TouristController.java
+++ flight-service/src/main/java/com/pickpack/flightservice/controller/tourist/TouristController.java
@@ -4,6 +4,7 @@ import com.pickpack.flightservice.api.response.ListRes;
 import com.pickpack.flightservice.entity.Tourist;
 import com.pickpack.flightservice.service.tourist.TouristService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +17,14 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/flight")
+@Slf4j
 public class TouristController {
     private final TouristService touristService;
 
     @GetMapping(value ="/tourist/{continent}", produces="application/json;charset=UTF-8")
     public ResponseEntity<?> getTouristsByContinent(@PathVariable("continent") String continent){
         List<Tourist> touristList = touristService.getTouristsByContinent(continent);
-        
+        log.info("해당 대륙의 관광지의 갯수! : {}", touristList.size());
         if(touristList.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
