31aa9d4feae97b206699909f33515ab2fd5fb8ea
seunghee <kanada11416@gmail.com>
2023-04-07 06:46:05 +0900

    chore : item complete에서 memberId-> member nickname으로 변경

--- item-service/src/main/java/com/pickpack/itemservice/repository/member/MemberRepository.java
+++ item-service/src/main/java/com/pickpack/itemservice/repository/member/MemberRepository.java
@@ -4,4 +4,5 @@ import com.pickpack.itemservice.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByNickname(String nickname);
 }
