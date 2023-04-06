1fdb6fd23585a9aac3be606c015dce54368cbf94
YoonHwe <dbsghl676@gmail.com>
2023-04-07 05:26:35 +0900

    feat : 항공권 왕복 연결

--- front/src/pages/pick/list/data.js
+++ front/src/pages/pick/list/data.js
@@ -1,3 +1,19 @@
+import garudaIndonesia from '../../../assets/airlines/가루다인도네시아항공.png'
+import gilsang from '../../../assets/airlines/길상항공.png'
+import korean from '../../../assets/airlines/대한항공.png'
+import delta from '../../../assets/airlines/델타항공.png'
+import cathay from '../../../assets/airlines/캐세이퍼시픽.png'
+import turkish from '../../../assets/airlines/터키항공.png'
+import fin from '../../../assets/airlines/핀에어.png'
+import lotpoland from '../../../assets/airlines/LOT폴란드항공.png'
+import eva from '../../../assets/airlines/에바항공.png'
+import klm from '../../../assets/airlines/KLM네덜란드항공.png'
+import asiana from '../../../assets/airlines/아시아나항공.png'
+import qatar from '../../../assets/airlines/카타르항공.png'
+import lao from '../../../assets/airlines/라오항공.png'
+import latam from '../../../assets/airlines/라탐항공.png'
+import royalbru from '../../../assets/airlines/로얄브루나이.png'
+import lufthanza from '../../../assets/airlines/루프트한자.png'
 const airlineData = [
     {
       airline: "가루다인도네시아항공",
@@ -16,9 +32,134 @@ const airlineData = [
     },
     {
       airline: "델타항공",
-      color: "rgb(255,255,255)",
+      color: "rgb(125,125,125)",
       image: delta,
     },
+    {
+      airline: "캐세이퍼시픽",
+      color: "#035C51",
+      image: cathay,
+    },
+    {
+      airline: "터키항공",
+      color: "#F14542",
+      image: turkish,
+    },
+    {
+      airline: "핀에어",
+      color: "#027DC5",
+      image: fin,
+    },
+    {
+      airline: "LOT폴란드항공",
+      color: "#0B2D70",
+      image: lotpoland,
+    },
+    {
+      airline: "에바항공",
+      color: "#31A357",
+      image: eva,
+    },
+    {
+      airline: "KLM네덜란드항공",
+      color: "#159EDA",
+      image: klm,
+    },
+    {
+      airline: "아시아나항공",
+      color: "#E92F2F",
+      image: asiana,
+    },
+    {
+      airline: "카타르항공",
+      color: "#873351",
+      image: qatar,
+    },
+    {
+      airline: "라오항공",
+      color: "#87C53F",
+      image: lao,
+    },
+    {
+      airline: "라탐항공",
+      color: "#704DF7",
+      image: latam,
+    },
+    {
+      airline: "로얄브루나이",
+      color: "#FFE600",
+      image: royalbru,
+    },
+    {
+      airline: "루프트한자",
+      color: "#FFB202",
+      image: lufthanza,
+    },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
+    // {
+    //   airline: "",
+    //   color: "",
+    //   image: ,
+    // },
   ];
 
   export {airlineData};
