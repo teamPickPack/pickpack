9e1bcdff0a3e4e9d3dbf1287e88ed1d3ed22ce61
seunghee <kanada11416@gmail.com>
2023-04-06 22:07:16 +0900

    chore : tendency 반환 형식 변경

--- flight-service/src/main/java/com/pickpack/flightservice/controller/tendency/TendencyController.java
+++ flight-service/src/main/java/com/pickpack/flightservice/controller/tendency/TendencyController.java
@@ -1,6 +1,7 @@
 package com.pickpack.flightservice.controller.tendency;
 
 import com.pickpack.flightservice.api.response.TendencyRes;
+import com.pickpack.flightservice.dto.tendency.TendencyInfoDto;
 import com.pickpack.flightservice.service.tendency.TendencyService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/flight")
@@ -17,13 +20,13 @@ public class TendencyController {
 
     private final TendencyService tendencyService;
 
-    @GetMapping("/{ticket_id}")
+    @GetMapping(value = "/{ticket_id}")
     public ResponseEntity<?> getTendency(@PathVariable("ticket_id") Long ticketId){
-
         TendencyRes tendencyRes = tendencyService.getTendency(ticketId);
         if(tendencyRes == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("no Info", HttpStatus.OK);
         }
+        System.out.println(tendencyRes.toString());
         return new ResponseEntity<>(tendencyRes, HttpStatus.OK);
     }
 }
