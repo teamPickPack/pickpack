ae4ffbd8abf89bf146e70056fab0ab9947913ec1
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-06 15:49:50 +0900

    feat: dsaasdffff

--- flight-service/src/main/java/com/pickpack/flightservice/service/tourist/TouristService.java
+++ flight-service/src/main/java/com/pickpack/flightservice/service/tourist/TouristService.java
@@ -4,6 +4,7 @@ import com.pickpack.flightservice.entity.Continent;
 import com.pickpack.flightservice.entity.Tourist;
 import com.pickpack.flightservice.repository.tourist.TouristRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 public class TouristService {
     private final TouristRepository touristRepository;
 
+    @Cacheable(value = "List<Tourist>", key = "#continent")
     public List<Tourist> getTouristsByContinent(String continent){
         return touristRepository.findByContinent(getContinent(continent));
     }
